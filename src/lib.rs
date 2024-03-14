19b1a5cb3f1883f2ed338ea2be64381277a8c382
William <drewlikesramen@gmail.com>
2024-03-14 13:25:48 -0400

    Used crossterm instead. Got snake to move.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,10 @@
+use std::io::Stdout;
+use std::ops::Add;
+
+use crossterm::cursor::MoveTo;
+use crossterm::style::Print;
+use crossterm::QueueableCommand;
+
 /// A board that contains the game of snake.
 pub struct Board {
     width: u32,
@@ -21,57 +28,66 @@ impl Board {
         }
     }
 
-    /// Renders game board to internal frame buffer.
-    /// First, clears frame buffer.
-    /// Then, renders snake.
-    /// Then, renders food character.
-    pub fn render_chars(&self, frame_buffer: &mut [char], palette: CharPalette) {
-        if frame_buffer.len() != (self.width * self.height) as usize {
-            panic!("Incorrect frame buffer size");
-        }
-        for c in frame_buffer.iter_mut() {
-            *c = palette.clear_char;
-        }
+    /// Renders game board to stdout.
+    pub fn render_to_stdout(&self, stdout: &mut Stdout, palette: CharPalette) {
+
+        // Clears screen
+        for y in 0..self.height {
+            stdout.queue(MoveTo(0, y as u16)).unwrap();
+            for _ in 0..self.width {
+                stdout.queue(Print(palette.empty_char)).unwrap();
+                stdout.queue(Print(palette.clear_char)).unwrap();
+            }
+        };
+
+        // Renders snake
         for snake_pos in self.snake.positions.iter().copied() {
-            if let Some(index) = self.index_of_pos(snake_pos) {
-                frame_buffer[index] = palette.snake_char;
+            if self.is_in_bounds(snake_pos) {
+                stdout.queue(MoveTo(snake_pos.x as u16 * 2, snake_pos.y as u16)).unwrap();
+                stdout.queue(Print(palette.snake_char)).unwrap();
+                stdout.queue(Print(palette.clear_char)).unwrap();
             }
         }
+
+        // Renders food
         let food_pos = self.food.position;
-        if let Some(index) = self.index_of_pos(food_pos) {
-            frame_buffer[index] = palette.food_char;
+        if  self.is_in_bounds(food_pos) {
+            stdout.queue(MoveTo(food_pos.x as u16 * 2, food_pos.y as u16)).unwrap();
+            stdout.queue(Print(palette.food_char)).unwrap();
+            stdout.queue(Print(palette.clear_char)).unwrap();
         }
     }
 
-    pub fn update(&mut self) -> GameState {
-        self.state
+    pub fn update(&mut self) {
+        let snake_head_pos = &mut self.snake.positions[0];
+        *snake_head_pos = *snake_head_pos + self.snake.direction.to_delta();
+        for i in 1..self.snake.positions.len()-1 {
+            self.snake.positions[i] = self.snake.positions[i-1];
+        }
     }
 
     /// Width and height of board.
-    pub fn size(&self) -> (u32, u32) {
-        (self.width, self.height)
-    }
+    pub fn size(&self) -> (u32, u32) { (self.width, self.height) }
+
+    /// State of the game
+    pub fn state(&self) -> GameState { self.state }
 
     /// Converts a board position into an index into the frame buffer.
     /// Returns [None] if out of bounds.
-    fn index_of_pos(&self, pos: Position) -> Option<usize> {
-        if pos.x < 0 || pos.y < 0 || pos.x as u32 >= self.width || pos.y as u32 >= self.height {
-            return None;
-        }
-        let (x, y, w) = (pos.x as usize, pos.y as usize, self.width as usize);
-        Some(y*w + x)
+    fn is_in_bounds(&self, pos: Vec2) -> bool {
+        pos.x >= 0 && pos.x < self.width as i32 && pos.y >= 0 && pos.y < self.height as i32
     }
 }
 
 /// Snake game object.
 #[derive(Clone, Eq, PartialEq, Debug)]
 pub struct Snake {
-    positions: Vec<Position>,
+    positions: Vec<Vec2>,
     direction: Direction,
 }
 
 impl Snake {
-    pub fn new(position: Position, direction: Direction) -> Self {
+    pub fn new(position: Vec2, direction: Direction) -> Self {
         Self {
             positions: vec![position],
             direction,
@@ -82,29 +98,51 @@ impl Snake {
 /// Food game object.
 #[derive(Copy, Clone, Eq, PartialEq, Default, Debug)]
 pub struct Food {
-    pub position: Position
+    pub position: Vec2
 }
 
 impl Food {
-    pub fn new(position: Position) -> Self {
+    pub fn new(position: Vec2) -> Self {
         Self { position }
     }
 }
 
-/// Primitive 2D position for game objects.
+/// Primitive 2D vector for game objects.
 #[derive(Copy, Clone, Eq, PartialEq, Default, Debug)]
-pub struct Position {
+pub struct Vec2 {
     pub x: i32,
     pub y: i32
+}
+
+impl Add for Vec2 {
+    type Output = Vec2;
+    fn add(self, rhs: Self) -> Self::Output {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
 }
 
 /// Direction the snake is facing.
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub enum Direction { N, S, E, W }
 
+impl Direction {
+    pub fn to_delta(self) -> Vec2 {
+        match self {
+            Direction::N => Vec2 { x: 0, y: -1 },
+            Direction::S => Vec2 { x: 0, y: 1 },
+            Direction::E => Vec2 { x: 1, y: 0 },
+            Direction::W => Vec2 { x: -1, y: 0 },
+        }
+    }
+}
+
 /// Palette to use when rendering characters.
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
 pub struct CharPalette {
+    pub empty_char: char,
     pub clear_char: char,
     pub snake_char: char,
     pub food_char: char,
@@ -113,7 +151,8 @@ pub struct CharPalette {
 impl Default for CharPalette {
     fn default() -> Self {
         Self {
-            clear_char: '.',
+            empty_char: '.',
+            clear_char: ' ',
             snake_char: 'S',
             food_char: 'F',
         }
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,32 @@
-use snake::{Board, CharPalette, Direction, Food, Position, Snake};
+use std::io::{stdout, Write};
+use std::time::Duration;
 
+use crossterm::terminal::{Clear, ClearType};
+use crossterm::QueueableCommand;
+use snake::{Board, CharPalette, Direction, Food, Vec2, Snake};
+
+const SLEEP_TIME: Duration = Duration::from_millis(500);
 const BOARD_WIDTH: u32 = 10;
 const BOARD_HEIGHT: u32 = 10;
-const SEPARATION_CHAR: char = ' ';
 
 fn main() {
-    
+
+    // Clears terminal
+    let mut stdout = stdout();
+    stdout.queue(Clear(ClearType::All)).unwrap();
+    stdout.flush().unwrap();
+
     // Creates snake board
-    let snake = Snake::new(Position { x: 0, y: 0 }, Direction::E);
-    let food = Food::new(Position { x: 5, y: 5 });
+    let snake = Snake::new(Vec2 { x: 0, y: 0 }, Direction::S);
+    let food = Food::new(Vec2 { x: 5, y: 5 });
     let mut board = Board::new(BOARD_WIDTH, BOARD_HEIGHT, snake, food);
 
-    // Updates and renders board
-    let mut frame_buffer = ['.'; (BOARD_WIDTH * BOARD_HEIGHT) as usize];
-    board.render_chars(&mut frame_buffer, CharPalette::default());
-    print_frame_buffer(&frame_buffer, BOARD_WIDTH, BOARD_HEIGHT);
-    board.update();
-}
-
-
-fn print_frame_buffer(frame_buffer: &[char], width: u32, height: u32) {
-    let mut i = 0;
-    for _ in 0..height {
-        for _ in 0..width {
-            let c = frame_buffer[i];
-            print!("{c}{SEPARATION_CHAR}");
-            i += 1;
-        }
-        println!();
+    // Runs game in loop.
+    // Renders to frame buffer, then renders frame buffer to CLI.
+    loop {
+        board.render_to_stdout(&mut stdout, CharPalette::default());
+        stdout.flush().unwrap();
+        std::thread::sleep(SLEEP_TIME);
+        board.update();
     }
 }
