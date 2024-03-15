a4953f0214519a8a8707f73ed3913b9d972b469c
William <drewlikesramen@gmail.com>
2024-03-15 00:39:23 -0400

    Rendering to framebuffer before printing

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
-use std::io::Stdout;
 use std::ops::Add;
 
-use crossterm::cursor::MoveTo;
-use crossterm::style::Print;
-use crossterm::QueueableCommand;
+const GAMEOVER_TEXT_LINE_1: &str = "Game Over";
+const GAMEOVER_TEXT_LINE_2: &str = "Gabe would have";
+const GAMEOVER_TEXT_LINE_3: &str = "done better!";
 
 /// A board that contains the game of snake.
 pub struct Board {
@@ -11,7 +10,8 @@ pub struct Board {
     height: u32,
     snake: Snake,
     food: Food,
-    state: GameState,
+    state: State,
+    initial_state: InitialState,
 }
 
 impl Board {
@@ -19,63 +19,139 @@ impl Board {
     /// Allocates game board
     pub fn new(width: u32, height: u32, snake: Snake, food: Food) -> Self {
         if width == 0 || height == 0 { panic!("Invalid board size {width}x{height}") }
+        let initial_state = InitialState {
+            snake_pos: snake.positions[0],
+            snake_dir: snake.direction,
+            food_pos: food.position,
+        };
         Self {
             width,
             height,
             snake,
             food,
-            state: GameState::Running,
+            state: State::Running,
+            initial_state,
         }
     }
 
     /// Renders game board to stdout.
-    pub fn render_to_stdout(&self, stdout: &mut Stdout, palette: CharPalette) {
-
-        // Clears screen
-        for y in 0..self.height {
-            stdout.queue(MoveTo(0, y as u16)).unwrap();
-            for _ in 0..self.width {
-                stdout.queue(Print(palette.empty_char)).unwrap();
-                stdout.queue(Print(palette.clear_char)).unwrap();
-            }
-        };
-
-        // Renders snake
-        for snake_pos in self.snake.positions.iter().copied() {
-            if self.is_in_bounds(snake_pos) {
-                stdout.queue(MoveTo(snake_pos.x as u16 * 2, snake_pos.y as u16)).unwrap();
-                stdout.queue(Print(palette.snake_char)).unwrap();
-                stdout.queue(Print(palette.clear_char)).unwrap();
-            }
+    pub fn render(&self, chars: &mut [char], palette: CharPalette) {
+        for c in chars.iter_mut() {
+            *c = palette.empty_char;
         }
-
-        // Renders food
-        let food_pos = self.food.position;
-        if  self.is_in_bounds(food_pos) {
-            stdout.queue(MoveTo(food_pos.x as u16 * 2, food_pos.y as u16)).unwrap();
-            stdout.queue(Print(palette.food_char)).unwrap();
-            stdout.queue(Print(palette.clear_char)).unwrap();
+        match self.state {
+            State::Running => self.render_running(chars, palette),
+            State::GameOver => self.render_game_over(chars),
+            State::Quit => panic!("Cannot render a game that quit"),
         }
     }
 
-    pub fn update(&mut self) {
+    fn render_running(&self, chars: &mut [char], palette: CharPalette) {
+        for snake_pos in self.snake.positions.iter().copied() {
+            if let Some(index) = self.index_of_pos(snake_pos) {
+                chars[index] = palette.snake_char
+            }
+        }
+        let food_pos = self.food.position;
+        if let Some(index) = self.index_of_pos(food_pos) {
+            chars[index] = palette.food_char;
+        }
+    }
+
+    fn render_game_over(&self, chars: &mut [char]) {
+        let center_x = self.width as i32 / 2;
+        let line_1 = Vec2 {
+            x: (center_x - GAMEOVER_TEXT_LINE_1.len() as i32 / 2 - 1).max(0) as i32,
+            y: self.height as i32 / 2 - 2,
+        };
+        let line_2 = Vec2 {
+            x: (center_x - GAMEOVER_TEXT_LINE_2.len() as i32 / 2 - 1).max(0) as i32,
+            y: self.height as i32 / 2 + 2,
+        };
+        let line_3 = Vec2 {
+            x: (center_x - GAMEOVER_TEXT_LINE_3.len() as i32 / 2 - 1).max(0) as i32,
+            y: self.height as i32 / 2 + 3,
+        };
+        self.render_text(chars, GAMEOVER_TEXT_LINE_1, line_1);
+        self.render_text(chars, GAMEOVER_TEXT_LINE_2, line_2);
+        self.render_text(chars, GAMEOVER_TEXT_LINE_3, line_3);
+        self.render_text(chars, "Press 'q' to quit", Vec2 { x: 0, y: self.height as i32 - 2});
+        self.render_text(chars, "Press 'r' to retry", Vec2 { x: 0, y: self.height as i32 - 1});
+    }
+
+    fn render_text(&self, chars: &mut [char], text: &str, pos: Vec2) {
+        for (i, c) in text.chars().enumerate() {
+            let c_pos = Vec2 { x: pos.x + i as i32, y: pos.y };
+            let Some(c_index) = self.index_of_pos(c_pos) else { return };
+            chars[c_index] = c;
+        }
+    }
+
+    pub fn update(&mut self, input: Option<Input>) {
+        match self.state {
+            State::Running => self.update_running(input),
+            State::GameOver => self.update_game_over(input),
+            State::Quit => panic!("Cannot update a game that quit"),
+        }
+    }
+
+    fn update_running(&mut self, input: Option<Input>) {
+        match input {
+            Some(Input::FaceN) => self.snake.direction = Direction::N,
+            Some(Input::FaceS) => self.snake.direction = Direction::S,
+            Some(Input::FaceE) => self.snake.direction = Direction::E,
+            Some(Input::FaceW) => self.snake.direction = Direction::W,
+            _ => {}
+        }
+
+        // Moves snake
         let snake_head_pos = &mut self.snake.positions[0];
         *snake_head_pos = *snake_head_pos + self.snake.direction.to_delta();
         for i in 1..self.snake.positions.len()-1 {
             self.snake.positions[i] = self.snake.positions[i-1];
         }
+
+        // Checks if snake is out of bounds or colliding with itself
+        let snake_head_pos = self.snake.positions[0];
+        if !self.in_bounds(snake_head_pos) {
+            self.state = State::GameOver;
+        }
+    }
+
+    fn update_game_over(&mut self, input: Option<Input>) {
+        match input {
+            Some(Input::Retry) => self.retry(),
+            Some(Input::Quit) => self.state = State::Quit,
+            _ => {}
+        }
+    }
+
+    fn retry(&mut self) {
+        self.state = State::Running;
+        self.snake.positions.clear();
+        self.snake.positions.push(self.initial_state.snake_pos);
+        self.snake.direction = self.initial_state.snake_dir;
+        self.food.position = self.initial_state.food_pos;
     }
 
     /// Width and height of board.
     pub fn size(&self) -> (u32, u32) { (self.width, self.height) }
 
     /// State of the game
-    pub fn state(&self) -> GameState { self.state }
+    pub fn state(&self) -> State { self.state }
 
     /// Converts a board position into an index into the frame buffer.
     /// Returns [None] if out of bounds.
-    fn is_in_bounds(&self, pos: Vec2) -> bool {
-        pos.x >= 0 && pos.x < self.width as i32 && pos.y >= 0 && pos.y < self.height as i32
+    fn index_of_pos(&self, pos: Vec2) -> Option<usize> {
+        if pos.x < 0 || pos.y < 0 || pos.x >= self.width as i32 || pos.y >= self.height as i32 {
+            return None;
+        }
+        let (x, y, width) = (pos.x as usize, pos.y as usize, self.width as usize);
+        Some(y * width + x)
+    }
+
+    fn in_bounds(&self, pos: Vec2) -> bool {
+        pos.x >=0 && pos.y >= 0 && pos.x < self.width as i32 && pos.y < self.height as i32
     }
 }
 
@@ -159,8 +235,29 @@ impl Default for CharPalette {
     }
 }
 
+/// State of the game.
 #[derive(Copy, Clone, Eq, PartialEq, Debug)]
-pub enum GameState {
+pub enum State {
     Running,
-    Dead,
+    GameOver,
+    Quit,
+}
+
+/// User input.
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+pub enum Input {
+    FaceN,
+    FaceS,
+    FaceE,
+    FaceW,
+    Retry,
+    Quit,
+}
+
+/// Initial state of the game.
+#[derive(Copy, Clone, Eq, PartialEq, Debug)]
+struct InitialState {
+    snake_pos: Vec2,
+    snake_dir: Direction,
+    food_pos: Vec2,
 }
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,89 @@
-use std::io::{stdout, Write};
+use std::io::{stdout, Stdout, Write};
 use std::time::Duration;
-
+use crossterm::cursor::{Hide, MoveTo};
+use crossterm::event::{poll, read, Event, KeyCode, KeyEvent, KeyEventKind};
+use crossterm::style::Print;
 use crossterm::terminal::{Clear, ClearType};
 use crossterm::QueueableCommand;
-use snake::{Board, CharPalette, Direction, Food, Vec2, Snake};
+use snake::{Board, CharPalette, Input, Direction, Food, Snake, State, Vec2};
 
-const SLEEP_TIME: Duration = Duration::from_millis(500);
-const BOARD_WIDTH: u32 = 10;
-const BOARD_HEIGHT: u32 = 10;
+const SLEEP_TIME: Duration = Duration::from_millis(100);
+const BOARD_WIDTH: u32 = 31;
+const BOARD_HEIGHT: u32 = 21;
 
 fn main() {
 
-    // Clears terminal
-    let mut stdout = stdout();
-    stdout.queue(Clear(ClearType::All)).unwrap();
-    stdout.flush().unwrap();
-
-    // Creates snake board
+    // Creates snake board.
     let snake = Snake::new(Vec2 { x: 0, y: 0 }, Direction::S);
     let food = Food::new(Vec2 { x: 5, y: 5 });
     let mut board = Board::new(BOARD_WIDTH, BOARD_HEIGHT, snake, food);
 
-    // Runs game in loop.
-    // Renders to frame buffer, then renders frame buffer to CLI.
+    // Hides cursor and clears terminal
+    let mut stdout = stdout();
+    stdout
+        .queue(Clear(ClearType::All)).unwrap()
+        .queue(Hide).unwrap()
+        .flush().unwrap();
+    
+
+    // Renders board and updates it
+    let mut frame_buffer = [' '; BOARD_WIDTH as usize * BOARD_HEIGHT as usize];
     loop {
-        board.render_to_stdout(&mut stdout, CharPalette::default());
-        stdout.flush().unwrap();
+        print_board(&mut board, &mut frame_buffer);
         std::thread::sleep(SLEEP_TIME);
-        board.update();
+        update_board(&mut board);
+        if board.state() == State::Quit {
+            break
+        }
     }
+}
+
+fn print_board(board: &mut Board, frame_buffer: &mut [char]) {
+    let mut stdout = stdout();
+    board.render(frame_buffer, CharPalette::default());
+    print_frame_buffer(&mut stdout, &frame_buffer, BOARD_WIDTH, BOARD_HEIGHT);
+}
+
+fn update_board(board: &mut Board) {
+    let input = read_keyboard_input();
+    board.update(input);
+}
+
+fn read_keyboard_input() -> Option<Input> {
+    if poll(Duration::ZERO).unwrap() {
+        match read().unwrap() {
+            Event::Key(key_event) => match key_event {
+                KeyEvent { code: KeyCode::Up, kind: KeyEventKind::Press, .. } => return Some(Input::FaceN),
+                KeyEvent { code: KeyCode::Down, kind: KeyEventKind::Press, .. } => return Some(Input::FaceS),
+                KeyEvent { code: KeyCode::Right, kind: KeyEventKind::Press, .. } => return Some(Input::FaceE),
+                KeyEvent { code: KeyCode::Left, kind: KeyEventKind::Press, .. } => return Some(Input::FaceW),
+                KeyEvent { code: KeyCode::Char('w'), kind: KeyEventKind::Press, .. } => return Some(Input::FaceN),
+                KeyEvent { code: KeyCode::Char('s'), kind: KeyEventKind::Press, .. } => return Some(Input::FaceS),
+                KeyEvent { code: KeyCode::Char('d'), kind: KeyEventKind::Press, .. } => return Some(Input::FaceE),
+                KeyEvent { code: KeyCode::Char('a'), kind: KeyEventKind::Press, .. } => return Some(Input::FaceW),
+                KeyEvent { code: KeyCode::Char('q'), kind: KeyEventKind::Press, .. } => return Some(Input::Quit),
+                KeyEvent { code: KeyCode::Char('r'), kind: KeyEventKind::Press, .. } => return Some(Input::Retry),
+                _ => {}
+            },
+            _ => {}
+        };
+    }
+    None
+}
+
+fn print_frame_buffer(stdout: &mut Stdout, chars: &[char], width: u32, height: u32) {
+    let mut i = 0;
+    let mut string = String::new();
+    for _ in 0..height {
+        for _ in 0..width {
+            let c = chars[i];
+            string.push(c);
+            string.push(' ');
+            i += 1;
+        }
+        string.push('\n');
+    }
+    stdout
+        .queue(MoveTo(0, 0)).unwrap()
+        .queue(Print(string)).unwrap();
 }
